891f80bdca2b02da2dc541bcf87aa6f2a9bf8e2b
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-19 01:21:38 +0800

    Migration: adding project table

---
+++ database/migrations/2023_08_18_040918_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('status')->nullable();
+            $table->string('priority')->nullable();
+            $table->timestamp('start_date')->nullable();
+            $table->timestamp('deadline')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};

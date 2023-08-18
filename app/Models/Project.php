2359e61866ef9b66b27401e17dfa6af35136b3ec
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-19 01:20:49 +0800

    Model: Project relationship with User
    Project belongsTo user

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    // protected $fillable = [
+    //     'name', 'status', 'owner', 'priority'
+    // ];
+
+    // protected $casts = [
+    //     'start_date' => 'datetime: Y-m-d',
+    //     'deadline' => 'datetime: Y-m-d'
+    // ]
+
+    // Relationship to User
+        public function user(): BelongsTo{
+            return $this->belongsTo(User::class, 'user_id');
+        }
+}

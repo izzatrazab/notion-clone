853c923248c85fe90c1e2e6dc5d0009e1005f37f
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-20 12:30:59 +0800

    Component: Project: implement editorjs/table
    using dummy data

--- resources/js/Components/Project/Project.vue
+++ resources/js/Components/Project/Project.vue
@@ -1,16 +1,64 @@
 <script setup>
+import EditorJS from '@editorjs/editorjs';
 import Table from '@editorjs/table';
+import { onMounted } from 'vue';
 
-var editor = EditorJS({
-  tools: {
-    table: Table,
-  }
-});
 
 </script>
 <template>
-test
+    {{ saved }}
+    <button @click='saveEditor()'>
+        save
+    </button>
+    <div id='editorjs'>
+    </div>
 </template>
 
-<style>
-</style>
+<style></style>
+<script>
+export default {
+    data() {
+        return {
+            editor: null,
+            saved: null
+        }
+    },
+    methods: {
+        saveEditor() {
+            this.editor.save().then((outputData) => {
+                this.saved = outputData.blocks
+            }).catch((error) => {
+                console.log('Saving failed: ', error)
+            });
+        },
+    },
+    mounted() {
+        this.editor = new EditorJS({
+            holder: 'editorjs',
+            tools: {
+                table: {
+                    class: Table,
+                    inlineToolbar: true,
+                    config: {
+                        // rows: 3,
+                        cols: 3,
+                        withHeadings: true,
+
+                    },
+                },
+            },
+            data: {
+                blocks:[
+                    {
+                        "type": "table",
+                        "data": {
+                            "withHeadings": true,
+                            "content": [["Kine", "Pigs", "Chicken"], ["1 pcs", "3 pcs", "12 pcs"], ["100$", "200$", "150$"]]
+                        }
+                    }
+                ],
+            }
+        })
+    }
+}
+</script>

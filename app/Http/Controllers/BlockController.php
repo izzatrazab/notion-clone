59ccce3586eb024bb40293ff7c2d9b71547ea959
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-21 09:49:42 +0800

    BlockController: add store function
    for now, store function will use updateOrCreate block

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -7,25 +7,22 @@ use Illuminate\Http\Request;
 
 class BlockController extends Controller
 {
-    // 
+    // fetch one block
     public function index()
     {
         $projects = Block::where('user_id', '=', '2');
         return $projects;
     }
 
-    public function store(Request $request, Block $blocks)
+    // create or update
+    public function store(Request $request)
     {
-        dd($request->json());
-        // Block::create($request->validate([
-        //     'json'=>'required',
-        // ]));
-        $product = Block::firstOrCreate(
-
-            [ 'name' => 'Platinum' ],
-
-            [ 'slug' => 'platinum', 'detail' => 'test platinum' ]
-
+        Block::updateOrCreate(
+            ['user_id'   => auth()->id()],
+            [
+                'type' => 'table',
+                'json' => $request->input('blocks')
+            ]
         );
     }
 }

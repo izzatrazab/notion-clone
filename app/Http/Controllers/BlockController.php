4031e7a3131204435476c93160bfdd0dd8507368
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-21 10:58:36 +0800

    BlockController: add show function
    show 1 block (Note: might change in the future)

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Block;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+
 
 class BlockController extends Controller
 {
@@ -12,6 +14,16 @@ class BlockController extends Controller
     {
         $projects = Block::where('user_id', '=', '2');
         return $projects;
+    }
+
+    // show
+    public function show(Request $request)
+    {
+        $blocks = Block::select('json')->where('user_id', auth()->id())->get();
+        return Inertia::render('Dashboard',[
+            'blocks' => $blocks
+        ]);
+        // return $blocks;
     }
 
     // create or update

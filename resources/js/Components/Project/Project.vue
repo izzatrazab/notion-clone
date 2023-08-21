d1298ad91385ac0c7f450486cf68292c9dce0f57
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-21 08:35:48 +0800

    implement typescript and try editorjs save function (only console log)

--- resources/js/Components/Project/Project.vue
+++ resources/js/Components/Project/Project.vue
@@ -1,12 +1,58 @@
-<script setup>
+<script setup lang='ts'>
 import EditorJS from '@editorjs/editorjs';
 import Table from '@editorjs/table';
-import { onMounted } from 'vue';
+import { reactive, onMounted } from 'vue'
+import { router } from '@inertiajs/vue3'
 
+const editor = new EditorJS({
+    holder: 'editorjs',
+    tools: {
+        table: {
+            class: Table,
+            inlineToolbar: true,
+            config: {
+                // rows: 3,
+                cols: 3,
+                withHeadings: true,
+            },
+        },
+    },
+    data: {
+        blocks: [
+            {
+                "type": "table",
+                "data": {
+                    "withHeadings": true,
+                    "content": [["Kine", "Pigs", "Chicken"], ["1 pcs", "3 pcs", "12 pcs"], ["100$", "200$", "150$"]]
+                }
+            },
+            {
+                "id": "123",
+                "type": "table",
+                "data": {
+                    "withHeadings": true,
+                    "content": [["Kine", "Pigs", "Chicken"], ["1 pcs", "3 pcs", "12 pcs"], ["100$", "200$", "150$"]]
+                }
+            }
+        ],
+    }
+})
+
+const form = reactive({
+    blocks: []
+})
+
+function saveEditor() {
+    editor.save().then((outputData) => {
+        form.blocks = [...form.blocks, ...outputData.blocks]
+        // router.post('/api/block', this.form)
+    }).catch((error) => {
+        console.log('Saving failed: ', error)
+    });
+}
 
 </script>
 <template>
-    {{ saved }}
     <button @click='saveEditor()'>
         save
     </button>
@@ -15,50 +61,3 @@ import { onMounted } from 'vue';
 </template>
 
 <style></style>
-<script>
-export default {
-    data() {
-        return {
-            editor: null,
-            saved: null
-        }
-    },
-    methods: {
-        saveEditor() {
-            this.editor.save().then((outputData) => {
-                this.saved = outputData.blocks
-            }).catch((error) => {
-                console.log('Saving failed: ', error)
-            });
-        },
-    },
-    mounted() {
-        this.editor = new EditorJS({
-            holder: 'editorjs',
-            tools: {
-                table: {
-                    class: Table,
-                    inlineToolbar: true,
-                    config: {
-                        // rows: 3,
-                        cols: 3,
-                        withHeadings: true,
-
-                    },
-                },
-            },
-            data: {
-                blocks:[
-                    {
-                        "type": "table",
-                        "data": {
-                            "withHeadings": true,
-                            "content": [["Kine", "Pigs", "Chicken"], ["1 pcs", "3 pcs", "12 pcs"], ["100$", "200$", "150$"]]
-                        }
-                    }
-                ],
-            }
-        })
-    }
-}
-</script>

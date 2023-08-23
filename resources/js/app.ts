f32d4c9aa04f2c8674c6dd798960e7db3a7e83b4
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-23 09:24:41 +0800

    vuetify: set defaultTheme to dark

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -14,6 +14,9 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
     components,
     directives,
+    theme:{
+        defaultTheme:'dark'
+    }
 })
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';

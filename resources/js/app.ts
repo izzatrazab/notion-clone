0c17f111a3483e5d93e9d132ba8685b4d82b5122
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-23 08:40:43 +0800

    app.ts: setup vuetify

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,4 @@
-import './bootstrap';
+// import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
@@ -6,16 +6,27 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => <any>resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return <any> createApp({ render: () => h(App, props) })
+        return <any>createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el) 
+            .use(vuetify)
+            .mount(el)
     },
     progress: {
         color: '#4B5563',

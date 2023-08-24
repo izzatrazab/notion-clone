7be334b1cfb79be4248cb8757ead30eab6038ded
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-24 08:39:59 +0800

    Models: create model for memo

---
+++ app/Models/Memo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Memo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'json_memo'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}

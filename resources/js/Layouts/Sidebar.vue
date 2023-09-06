417f25429df385ef93f2d1b39121400da68de71a
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-09-06 23:28:54 +0800

    Sidebar: SidebarSwitchTeam component display only if has more than 1 team

--- resources/js/Layouts/Sidebar.vue
+++ resources/js/Layouts/Sidebar.vue
@@ -16,13 +16,13 @@ let rail = ref(true)
 <template>
     <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
         <v-list density="compact" nav>
-            <v-list-item :title="$page.props.jetstream.hasTeamFeatures ? $page.props.auth.user.current_team.name.toUpperCase() : ''" 
+            <v-list-item :title="$page.props.jetstream.hasTeamFeatures ? 'team: '+ $page.props.auth.user.current_team.name.toUpperCase() : ''" 
                 :prepend-icon="rail === true ? 'mdi-chevron-right' : 'mdi-chevron-left'" @click.stop="rail = !rail" />
         </v-list>
         <SidebarAccount />
         <template v-if="$page.props.jetstream.hasTeamFeatures">
             <SidebarTeam />
-            <SidebarSwitchTeam />
+            <SidebarSwitchTeam v-if='$page.props.auth.user.all_teams.length > 1'/>
         </template>
         <SidebarProject />
         <SidebarMemo />

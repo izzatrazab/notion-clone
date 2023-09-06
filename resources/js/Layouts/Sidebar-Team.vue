a0fd5c1a0889b1aed8a27558421816608f66299a
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-09-06 10:23:55 +0800

    Layout: prepare for Sidebar-Team component
    I really want to remove the top bar cause it is making the screen smaller.

---
+++ resources/js/Layouts/Sidebar-Team.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import { Link, router } from '@inertiajs/vue3';
+const logout = () => {
+    router.post(route('logout'));
+};
+
+</script>
+
+<template>
+    <v-list density="compact" nav>
+        <v-list-group value='Team'>
+            <template v-slot:activator="{ props }">
+                <v-list-item :title='$page.props.auth.user.current_team.name' v-bind="props" prepend-icon="mdi-account" />
+            </template>
+            <Link :href="route('teams.show', $page.props.auth.user.current_team)">
+            <v-list-item value='Setting' title='Settings' />
+            </Link>
+            <Link :href="route('teams.create')" v-if="$page.props.jetstream.canCreateTeams">
+            <v-list-item value='create-new-team' title='Create New Team' />
+            </Link>
+            <v-list-item value='log-out' @click="logout" title='Log Out' />
+        </v-list-group>
+    </v-list>
+</template>

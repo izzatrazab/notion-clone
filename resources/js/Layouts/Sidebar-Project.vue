cbcd695407e0034bebf39fb34737216e4e6e85e0
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-09-06 16:27:35 +0800

    Sidebar: Project: prepare project section

---
+++ resources/js/Layouts/Sidebar-Project.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { onMounted } from 'vue';
+import { router } from '@inertiajs/vue3';
+import axios from 'axios';
+
+let projectNameList
+
+async function getProjectNameList() {
+    const { data } = await axios.get('/api/project')
+    projectNameList = data.projects
+}
+
+async function createProject() {
+    // const { data } = await axios.post('/api/project')
+    // console.log(data);
+    // if (data.project_id) {
+    //     //success create new memo
+    //     router.visit('/project/' + data.project_id)
+    //     return
+    // }
+    // console.log('create new project failed');
+}
+
+onMounted(() => {
+    // getProjectNameList()
+})
+</script>
+
+<template>
+    <v-list density="compact" nav>
+        <v-list-group value='Project'>
+            <template v-slot:activator="{ props }">
+                <v-list-item v-bind="props" prepend-icon="mdi-chart-gantt" title='PROJECT' />
+            </template>
+            <v-list-item value='All Project' title='All Project' @click='router.visit("/project")' />
+            <v-list-item v-for='(projectName, index) in projectNameList' :value='"project " + index'
+                @click='router.visit("/project/" + projectName.id)' :title='projectName.name' />
+            <v-list-item append-icon="mdi-plus" title="create project" @click="createProject" />
+        </v-list-group>
+    </v-list>
+</template>

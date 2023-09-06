8542bbab783ef9f16ff27515d5ba340f5ab3edff
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-09-06 16:28:51 +0800

    Sidebar: code clean up and rearrange item's position

--- resources/js/Layouts/Sidebar.vue
+++ resources/js/Layouts/Sidebar.vue
@@ -1,23 +1,12 @@
 <script setup >
 import { ref } from 'vue';
-import { Link } from '@inertiajs/vue3';
-import axios from 'axios';
-import { onMounted } from 'vue';
 import SidebarMemo from './Sidebar-Memo.vue'
 import SidebarAccount from './Sidebar-Account.vue'
+import SidebarProject from './Sidebar-Project.vue';
 
 
 let drawer = ref(true)
 let rail = ref(true)
-let memoNameList
-
-async function getMemoNameList() {
-    const { data } = await axios.get('/api/memo')
-    memoNameList = data.memos
-}
-onMounted(() => {
-    getMemoNameList()
-})
 
 </script>
 
@@ -29,11 +18,11 @@ onMounted(() => {
                     @click.stop="rail = !rail"></v-btn>
             </template>
         </v-list-item>
-
         <v-divider />
         <SidebarAccount/>
         <v-divider />
-        
+        <SidebarProject/>
+        <v-divider />
         <SidebarMemo />
     </v-navigation-drawer>
 </template>

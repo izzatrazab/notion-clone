b1b3574242cf2f6948be102e97a14e70adf69a43
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-09-07 12:14:10 +0800

    Project: (tm)2, basically add a form for editing project

--- resources/js/Pages/Project/show.vue
+++ resources/js/Pages/Project/show.vue
@@ -1,9 +1,13 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue'
+import Gantt from 'frappe-gantt'
+import axios from 'axios';
 import { Link } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
-import Gantt from 'frappe-gantt'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
 
 const props = defineProps({
     project: {
@@ -11,6 +15,8 @@ const props = defineProps({
         default: "no id received"
     }
 })
+
+let date = ref([props.project.start, props.project.end])
 
 let gantt = null
 let tasks = reactive([{
@@ -33,7 +39,7 @@ let tasks = reactive([{
     id: '2',
     name: 'Revamp website no2',
     start: '2016-12-28',
-    end: '2016-12-31',
+    end: '2016-12-28',
     progress: 20,
     dependencies: '1'
 }])
@@ -73,6 +79,31 @@ onMounted(() => {
     }))
 })
 
+async function updateProject() {
+    let form = {
+        project_id: props.project.id,
+        name: props.project.name,
+        start: null,
+        end: null
+    }
+
+    try {
+        form.start = `${date.value[0].getFullYear()}-${date.value[0].getMonth() + 1}-${date.value[0].getDate()}`
+    } catch (error) {
+        form.start = date.value[0]
+    }
+    try {
+        form.end = `${date.value[1].getFullYear()}-${date.value[1].getMonth() + 1}-${date.value[1].getDate()}`
+    } catch (error) {
+        form.end = date.value[1]
+    }
+    try {
+        await axios.put('/project', form)
+    } catch (error) {
+        console.log('Update project failed: ', error);
+    }
+}
+
 </script>
 
 <template>
@@ -88,7 +119,20 @@ onMounted(() => {
             <v-expansion-panels>
                 <v-expansion-panel title="Details">
                     <v-expansion-panel-text>
-                        {{ props.project }}
+                        <v-form @submit.prevent='updateProject()'>
+                            <v-container>
+                                <v-row align='end'>
+                                    <v-col cols="12" md="4">
+                                        <v-text-field v-model="props.project.name" :counter="10" label="Project name"
+                                            hide-details></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" md="4">
+                                        <VueDatePicker v-model="date" range format='yyyy-MM-dd'></VueDatePicker>
+                                    </v-col>
+                                </v-row>
+                                <v-btn type="submit" color='primary' class="mt-2">update</v-btn>
+                            </v-container>
+                        </v-form>
                     </v-expansion-panel-text>
                 </v-expansion-panel>
             </v-expansion-panels>
@@ -139,14 +183,14 @@ onMounted(() => {
 
             </v-card>
         </section>
-        <v-divider :thickness="20" ></v-divider>
+        <v-divider :thickness="20"></v-divider>
         <section>
             <v-card>
                 <v-card-title>
                     Gantt Chart
                 </v-card-title>
                 <v-card-item>
-                    <svg id="gantt"/>
+                    <svg id="gantt" />
                 </v-card-item>
             </v-card>
         </section>
